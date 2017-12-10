848faffc395b2be52b818d92fabe2acba5e0a0f8
Denis Bobrovnikov <denis.bobrovnikov@gmail.com>
2017-12-10 23:37:46 +0300

    #issue-2 new dir structure draft

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
+var path = require('path');
 var io = require('socket.io')(http);
-var Events = require('shared/events');
+var Events = require('./shared/events');
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+var index = require('./routes/index');
+app.use(express.static(__dirname + '/views'));
 
 io.on(Events.CONNECTION, function(socket){
     socket.on(Events.CHAT_MESSAGE, function(msg){
